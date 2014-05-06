e8195096b6b5d8e7c1c8507413a6096562dc0c27
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-05-06 10:31:01 +0000

    aggiunta documentazione

--- manifests/newrelic.pp
+++ manifests/newrelic.pp
@@ -1,3 +1,33 @@
+# define: ispconfig_solr::newrelic
+#
+# This define is a wrapper of newrelic::java class. It install newrelic java agent
+#
+# Parameters:
+#
+# [*path*]
+#   Path where agent will be installed
+#
+# Global Variables
+#   Global variable newrelic_license_free_key will be used as newrelic license key.
+#   It can be overriden by defining newrelic_license_pro_key variable (ex: at cluster level or where solr instance is defined)
+#
+# Sample Usage of free license
+#
+#  node foo {
+#     newrelic::java {'appXYZ':
+#       path                        => "/opt/newrelic",
+#     }
+#  }
+#
+# Sample Usage of pro license
+#
+#  node foo {
+#     $newrelic_license_pro_key = 'YYYYYYYYYYYYY'
+#     newrelic::java {'appXYZ':
+#       path => "/opt/newrelic",
+#     }
+#  }
+#
 define ispconfig_solr::newrelic (
   $path,
 ) {
