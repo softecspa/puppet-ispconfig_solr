04d1d80f1f944d4b04eb1a328d054aec46d9917e
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-05-08 07:53:07 +0000

    fix newrelic

--- manifests/newrelic.pp
+++ manifests/newrelic.pp
@@ -37,21 +37,13 @@ define ispconfig_solr::newrelic (
   }
 
 
-  $newrelic_license_key = $newrelic_license_pro_key?{
-    ''      => $newrelic_license_free_key,
-    default => $newrelic_license_pro_key
-  }
-
   if !defined (Newrelic::Server[$hostname]) {
-    newrelic::server { $hostname:
-      newrelic_license_key => $newrelic_license_key,
-    }
+    newrelic::server { $hostname: }
   }
 
   if !defined(Class['newrelic::java']) {
     class{'newrelic::java':
       path                        => "$path/newrelic",
-      newrelic_license_key        => $newrelic_license_key,
       newrelic_java_conf_appname  => "Java Apps on $cluster",
     }
   }
